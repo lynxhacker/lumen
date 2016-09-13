01fbab130527b09ad42135a494c84df83d67a22b
lynxhacker <nahayo3@gmail.com>
2016-09-13 22:28:09 +0300

    CI test

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,5 +1,33 @@
 <?php 
-
+use App\Models\User; 
+use App\Models\Booking; 
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
-class UserTest extends
+class UserTest extends TestCase
+{
+	/*protected $user, $booking; 
+
+	public function __construct(User $user, Booking $booking)
+	{
+		$this->user = $user; 
+		$this->booking = $booking; 
+	}
+	
+	public function testUserWithBooking1AndId1 ()
+	{
+		$this->json('POST', '/user/1/booking/1', [])
+			 ->seeJson([
+			 	'user_id' => 1, 
+			 	'booking_id' => 1
+			]);
+
+		return $this->assertEquals(200, $this->response->status()); 
+	}*/
+
+	public function testUserConnect ()
+	{
+		$this->json('GET', '/'); 
+
+		return $this->assertEquals(200, $this->response->status()); 
+	}
+}
